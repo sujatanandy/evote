7af6c72e223281cb458bb16bfb11d4995ff99cef
sujatanandy <115167634+sujatanandy@users.noreply.github.com>
2022-10-07 22:15:54 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+	    <title>Online Voting System- Dashboard</title>
+	</head>
+	<body>
+	<button>Back</button>
+	<button>Logout</button>
+	    <h1>Online voting system</h1>
+		<hr>
+		<div id="Profile"></div>
+		<div id="Party"></div>
+	</body>
+</html>
